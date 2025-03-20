3a6f64f4ab03d3cff4a69f5f816d7fcacf21386c
sholademi daniel <sholademidaniel@gmail.com>
2025-03-20 19:49:51 +0100

    made chages in app.py:

--- app.py
+++ app.py
@@ -27,40 +27,46 @@ def binarizer(file):
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    try:
     
-    file= request.files['mri_scan']
-    print(type(file))
+        file= request.files['mri_scan']
+        print(type(file))
+        
+        temp_filepath = os.path.join(file.filename)
+        file.save(temp_filepath)
+        
+                    
+        loaded_image= load_img(temp_filepath, target_size=(256,256))
+        img_to_array= keras.preprocessing.image.img_to_array(loaded_image)
+        img_to_array_expanded= np.expand_dims(img_to_array, axis=0)
+        
+        interpreter= tf.lite.Interpreter(os.path.join(os.getcwd(), 'converted_model.tflite'))
+        interpreter.allocate_tensors()
+        
+        input_details= interpreter.get_input_details()
+        output_details= interpreter.get_output_details()
+        
+        interpreter.set_tensor(input_details[0]['index'], img_to_array_expanded)
+        
+        interpreter.invoke()
+        
+        
+        result = interpreter.get_tensor(output_details[0]['index'])
+        result= np.argmax(result)
+        class_name= ["Benign", "Malignant", "Healthy"]
+        prediction= class_name[result]
+        print(prediction)
+        
     
-    temp_filepath = os.path.join(file.filename)
-    file.save(temp_filepath)
+        
+        os.remove(temp_filepath)
+        
+        return render_template('index.html', prediction= prediction)
     
-                
-    loaded_image= load_img(temp_filepath, target_size=(256,256))
-    img_to_array= keras.preprocessing.image.img_to_array(loaded_image)
-    img_to_array_expanded= np.expand_dims(img_to_array, axis=0)
-    
-    interpreter= tf.lite.Interpreter(os.path.join(os.getcwd(), 'converted_model.tflite'))
-    interpreter.allocate_tensors()
-    
-    input_details= interpreter.get_input_details()
-    output_details= interpreter.get_output_details()
-    
-    interpreter.set_tensor(input_details[0]['index'], img_to_array_expanded)
-    
-    interpreter.invoke()
-    
-    
-    result = interpreter.get_tensor(output_details[0]['index'])
-    result= np.argmax(result)
-    class_name= ["Benign", "Malignant", "Healthy"]
-    prediction= class_name[result]
-    print(prediction)
-    
- 
-    
-    os.remove(temp_filepath)
-    
-    return render_template('index.html', prediction= prediction)
+    except Exception as e:
+        print(f"error is {e}")
+        return render_template("index.html", prediction=None)
+        
     
 
     
